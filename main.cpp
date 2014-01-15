5f7de16ee853a675b2fbfedd3cf210a4d3e0873c
depzoo <depzoo@gmail.com>
2014-01-15 22:20:30 +0100

    List edges.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <vector>
+#include "defaultgr.h"
 
 using namespace std;
 
 int main() {
 
-  cout << "Hello world!" << endl;
-  return 0;
+  	cout << "Hello world!" << endl;
+	cout << "Default graph edges:" << endl;
+		for(int x = 0; x<4; x++)
+		{
+	 		cout << defaultgr[x] << " ";
+		}
+	cout << endl;
+	
+	
+return 0;
 }
 
 //created by Kockito Kuczko @2014
