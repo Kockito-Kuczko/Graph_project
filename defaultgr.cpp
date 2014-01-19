9f58a2a88c123bd903997c8cbfcbea2cd87cacc9
depzoo <depzoo@gmail.com>
2014-01-19 14:14:16 +0100

    sdafkjdslkfa

---
+++ defaultgr.cpp
@@ -0,0 +1,7 @@
+#include "constant.h"
+#include <vector>
+#include "defaultgr.h"
+
+
+int defaultgr[4] = {1,2,3,4};
+
--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
-#include <iostream>
-#include <vector>
+#include "constant.h"
 #include "defaultgr.h"
 
-using namespace std;
 
 int main() {
 
