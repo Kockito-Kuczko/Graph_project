17662d0dbea2573f0e914433495aca317916119f
depzoo <depzoo@gmail.com>
2014-01-19 15:20:34 +0100

    4

--- main.cpp
+++ main.cpp
@@ -8,9 +8,19 @@ int main() {
 	cout << "Default graph edges:" << endl;
 	for(int x = 0; x < 4; x++) {
 	    for (int y = 0; y < 2; y++){
-	        cout << "Enter adjacent for vertex " << x+1 << "," << y+1 << endl;
+	        cout << "Enter adjacent for vertex " << x + 1 << "," << y + 1 << endl;
 	        cin >> defaultgr[x][y] ;
+	        
+	        //Check if vertex number is valid
+	        if  (defaultgr[x][y] < 0 || defaultgr[x][y] > 3) {
+	            cout << "Error: vertex numbers range from 0 to 3" << endl;
+	        }
+	        
+	        
             //cout << defaultgr[x][y] << " ";
+        }
+        if (defaultgr[x][0] == defaultgr[x][1]) {
+            cout << "No loop edges allowed for now, please." << endl;
         }
 	}
 
