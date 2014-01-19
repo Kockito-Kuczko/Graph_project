be8ce55c6dc553f47e91f08f24121c02da4fcffd
depzoo <depzoo@gmail.com>
2014-01-19 17:10:33 +0100

    Added input checks.
    Known bugs:
    -checkInput function cannot handle inputs other than integers and will end the
    program prematurely.

--- main.cpp
+++ main.cpp
@@ -5,28 +5,30 @@ bool checkInput(char c);
 bool checkExistingEdge();
 
 int main() {
-	cout << "Hello world!" << endl;
+	cout << "Graph Utility v0.01" << endl;
 	
-	cout << "Default graph edges:" << endl;
+	cout << "\n Default graph edges:" << endl;
 	for(int x = 0; x < 4; x++) {
 	    for (int y = 0; y < 2; y++) {
-	        cout << "Enter adjacent for vertex " << x + 1 << "," << y + 1 << endl;
+	        cout << "Enter two adjacents for vertex " << x + 1 <<  endl;
 	        cin >> defaultgr[x][y];
 	        
-	        //Check if input is an integer
+	        //Force valid input
 	        if (checkInput(defaultgr[x][y]) == false) {
-	            cout << "Please enter an integer." << endl;
-	        }
+                cout << "Error! Please enter a number:" << endl;
+                y = y - 1;
+            }    
+            
+            if (defaultgr[x][y] < 0 || defaultgr[x][y] > 3) {
+	            cout << "Not a valid number. You must enter a number between 0 and " << "3" << ":" << endl;	            
+                y = y - 1;                
+            }
 	        
-	        //Check if input vertex number is valid
-	        if  (defaultgr[x][y] < 0 || defaultgr[x][y] > 3) {
-	            cout << "Error: vertex numbers range from 0 to 3" << endl;
-	        }
         }
         
-        //Warn about loop edges.
-        if (defaultgr[x][0] == defaultgr[x][1]) {
-            cout << "No loop edges allowed for now, please." << endl;
+        //Warn about loop edges
+        if ((defaultgr[x][0] || defaultgr[x][1]) == x) {
+            cout << "You have created at least one loop edge." << endl;
         }
 	}
 
@@ -35,8 +37,22 @@ int main() {
 	return 0;
 }
 
+/* input check prototype
+
+if (checkInput(defaultgr[x][y]) == false) {
+    cout << "Error! Please enter a number:" << endl;
+    y = y - 1;
+}    
+else {
+    if (defaultgr[x][y] < 0 || defaultgr[x][y] > 3) {
+	    cout << "Not a valid number. You must enter a number between 0 and " << "3" << ":" << endl;	            
+        y = y - 1;
+}
+
+*/
+
 bool checkInput(char c) {
-    return '0' <= c && c <= '9';
+    return 0 <= c && c <= 9;
 
 }
 
@@ -45,5 +61,3 @@ bool checkExistingEdge() {
 }
 
 //created by Kockito Kuczko @2014
-//gedit +terminal push test 2
-//pull test
