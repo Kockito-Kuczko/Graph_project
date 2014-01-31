2a03ca6f33d9e2108fe1fc03640c584683919711
depzoo <depzoo@gmail.com>
2014-01-31 17:21:24 +0100

    Added graph constructor with input checks.

--- main.cpp
+++ main.cpp
@@ -1,32 +1,51 @@
 //created by Kockito Kuczko @2014
 
 #include "constant.h"
-#include "defaultgr.h"
-#include <ctype.h>
-#include <string>
-
-bool checkIfNumber(string i);
-bool checkExistingEdge();
 
 int main() {
 	cout << "Graph Utility v0.01" << endl;
+    int graphSize, graphRealSize;
+	int maxAdj, maxRealAdj;
 
-	cout << "\n Default graph edges:" << endl;
-	for(int x = 0; x < 4; x++) {
-	    for (int y = 0; y < 2; y++) {
-	        cout << "Enter two adjacents for vertex " << x + 1 <<  endl;
+	cout << endl << "Enter the size of the graph (number of vertices):" << endl;
+	cin >> graphRealSize;
+	while (cin.fail()) {
+        cin.clear();
+        cin.ignore(256, '\n');
+        cout << "Error! Please enter an integer:" << endl;
+        cin >> graphRealSize;
+	}
+
+	graphSize = graphRealSize + 1;
+
+    cout << "Enter number of maximum adjacents of each vertex:" << endl;
+
+	cin >> maxRealAdj;
+	while (cin.fail() || maxRealAdj >= graphRealSize) {
+        cin.clear();
+        cin.ignore(256, '\n');
+        cout << "Error! Please enter an integer:" << endl;
+        cin >> maxRealAdj;
+	}
+
+	maxAdj = maxRealAdj + 1;
+
+	int defaultgr[graphSize][maxAdj];
+
+	for(int x = 1; x < graphSize; x++) {
+	    for (int y = 1; y < maxAdj; y++) {
+	        cout << "Enter adjacent for vertex " << x << ":" << endl;
 	        cin >> defaultgr[x][y];
 
 	        //Force valid input
-                if (cin.fail() ) {
-                    cout << "Error! Please enter a number:" << endl;
+                if (cin.fail()) {
+                    cout << "Error! Please enter an integer:" << endl;
                     cin.clear();
                     cin.ignore(256, '\n');
                     y = y - 1;
                 }
-
-                if (defaultgr[x][y] < 0 || defaultgr[x][y] > 3) {
-                    cout << "Not a valid number. You must enter a number between 0 and " << "3" << ":" << endl;
+                else if (defaultgr[x][y] < 1 || defaultgr[x][y] > graphRealSize) {
+                    cout << "No such vertex. You must enter a number between 1 and " << graphRealSize << ":" << endl;
                     y = y - 1;
                 }
 
@@ -36,19 +55,27 @@ int main() {
                 y = y-1;
                 }
 
+            //Warn about mupltiple edges
+                for (int i = 1; i < y; i++){
+                    if (defaultgr[x][i] == defaultgr[x][y]) {
+                        cout << "No multiple edges allowed, please enter another vertex:" << endl;
+                        y = y-1;
+                    }
+                }
+
 
         }
 	}
 
 	cout << endl << endl << "Adjacency list:" << endl;
 
-	for (int x = 0; x < 4; x++){
+	for (int x = 1; x < graphSize; x++){
 
-	    cout << endl << "Vertex " << x+1 << ":" << endl;
+	    cout << endl << "Vertex " << x << ":" << endl;
 
-        for (int y = 0; y < 2; y++){
+        for (int y = 1; y < maxAdj; y++){
             cout << defaultgr[x][y];
-            if (y < 1){
+            if (y < maxRealAdj){
                 cout << ", ";
             }
         }
@@ -57,8 +84,4 @@ int main() {
 	cout << endl;
 
 	return 0;
-}
-
-bool checkExistingEdge() {
-
 }
