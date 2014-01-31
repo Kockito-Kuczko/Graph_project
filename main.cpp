42e1712f3ff0005f715bd4951942c8be415c82b9
depzoo <depzoo@gmail.com>
2014-01-31 16:20:12 +0100

    Fixed input check funcionality. Program now prints adjacency list for feedback.

--- main.cpp
+++ main.cpp
@@ -1,63 +1,64 @@
+//created by Kockito Kuczko @2014
+
 #include "constant.h"
 #include "defaultgr.h"
+#include <ctype.h>
+#include <string>
 
-bool checkInput(char c);
+bool checkIfNumber(string i);
 bool checkExistingEdge();
 
 int main() {
 	cout << "Graph Utility v0.01" << endl;
-	
+
 	cout << "\n Default graph edges:" << endl;
 	for(int x = 0; x < 4; x++) {
 	    for (int y = 0; y < 2; y++) {
 	        cout << "Enter two adjacents for vertex " << x + 1 <<  endl;
 	        cin >> defaultgr[x][y];
-	        
+
 	        //Force valid input
-	        if (checkInput(defaultgr[x][y]) == false) {
-                cout << "Error! Please enter a number:" << endl;
-                y = y - 1;
-            }    
-            
-            if (defaultgr[x][y] < 0 || defaultgr[x][y] > 3) {
-	            cout << "Not a valid number. You must enter a number between 0 and " << "3" << ":" << endl;	            
-                y = y - 1;                
-            }
-	        
+                if (cin.fail() ) {
+                    cout << "Error! Please enter a number:" << endl;
+                    cin.clear();
+                    cin.ignore(256, '\n');
+                    y = y - 1;
+                }
+
+                if (defaultgr[x][y] < 0 || defaultgr[x][y] > 3) {
+                    cout << "Not a valid number. You must enter a number between 0 and " << "3" << ":" << endl;
+                    y = y - 1;
+                }
+
+            //Warn about loop edges
+                if ((defaultgr[x][y] == x)) {
+                cout << "No loop edges allowed, please enter another vertex:" << endl;
+                y = y-1;
+                }
+
+
         }
-        
-        //Warn about loop edges
-        if ((defaultgr[x][0] || defaultgr[x][1]) == x) {
-            cout << "You have created at least one loop edge." << endl;
+	}
+
+	cout << endl << endl << "Adjacency list:" << endl;
+
+	for (int x = 0; x < 4; x++){
+
+	    cout << endl << "Vertex " << x+1 << ":" << endl;
+
+        for (int y = 0; y < 2; y++){
+            cout << defaultgr[x][y];
+            if (y < 1){
+                cout << ", ";
+            }
         }
 	}
 
 	cout << endl;
-	
+
 	return 0;
-}
-
-/* input check prototype
-
-if (checkInput(defaultgr[x][y]) == false) {
-    cout << "Error! Please enter a number:" << endl;
-    y = y - 1;
-}    
-else {
-    if (defaultgr[x][y] < 0 || defaultgr[x][y] > 3) {
-	    cout << "Not a valid number. You must enter a number between 0 and " << "3" << ":" << endl;	            
-        y = y - 1;
-}
-
-*/
-
-bool checkInput(char c) {
-    return 0 <= c && c <= 9;
-
 }
 
 bool checkExistingEdge() {
 
 }
-
-//created by Kockito Kuczko @2014
